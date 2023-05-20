4ebf6806d57b851f7c71181edb48c310124907b3
Gabor Berta <Gabor.Berta@quest.com>
2023-05-20 12:20:02 +0200

    NewsLetterPage.java: added page where user can subscribe to newsletters

---
+++ tests/src/test/java/NewsLetterPage.java
@@ -0,0 +1,42 @@
+import org.junit.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+
+
+class NewsLetterPage extends PageBase {
+
+    private By headTitleBy = By.className("page-head-title");
+    //private By subscribeRadioBtn = By.id("newsletter-yes");
+    private By subscribeRadioBtn = By.xpath("//*[@id='newsletter']/div[1]/div/div[1]/label");
+    private By unSubscribeRadioBtn = By.xpath("//*[@id='newsletter']/div[1]/div/div[2]/label");
+    private By submitSubscribeBtn = By.xpath("//button[contains(text(), 'Continue')]");
+    
+    public NewsLetterPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getHeadTitleText() {
+        return this.waitAndReturnElement(headTitleBy).getText();
+    }
+
+    public LoggedInAccountPage subscribe() {
+        waitAndReturnElement(subscribeRadioBtn).click();
+        waitAndReturnElement(submitSubscribeBtn).click();
+        return new LoggedInAccountPage(driver);
+    }
+
+    public LoggedInAccountPage unsubscribe() {
+        waitAndReturnElement(unSubscribeRadioBtn).click();
+        waitAndReturnElement(submitSubscribeBtn).click();
+        return new LoggedInAccountPage(driver);
+    }
+
+}
