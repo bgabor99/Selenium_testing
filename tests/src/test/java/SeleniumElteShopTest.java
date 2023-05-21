f51ff11026d72e435d36f5e446e2a886e1164849
Gabor Berta <Gabor.Berta@quest.com>
2023-05-22 00:03:12 +0200

    Deleted debug print lines

--- tests/src/test/java/SeleniumElteShopTest.java
+++ tests/src/test/java/SeleniumElteShopTest.java
@@ -20,47 +20,36 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testMainPageFooter() {
-        System.out.println("---- testMainPageFooter ----");
         MainPage mainPage = new MainPage(this.driver);
-        //System.out.println(mainPage.getFooterText());
         Assert.assertTrue(mainPage.getFooterText().contains("Partners"));
         Assert.assertTrue(mainPage.getFooterText().contains("SHOP"));
     }
 
     @Test
     public void testMainPageTitle() {
-        System.out.println("---- testMainPageTitle ----");
         MainPage mainPage = new MainPage(this.driver);
-        //System.out.println(mainPage.driver.getTitle());
         Assert.assertTrue(mainPage.driver.getTitle().contains("ELTE Shop"));
     }
     
     @Test
     public void testSearchFound() {
-        System.out.println("---- testSearchFound ----");
         MainPage mainPage = new MainPage(this.driver);
-
         SearchResultPage searchResultPage = mainPage.search("polo");
         String bodyText = searchResultPage.getBodyText();
-        //System.out.println(bodyText);
         Assert.assertTrue(bodyText.contains("ADD TO CART"));
         Assert.assertTrue(bodyText.contains("BEAC"));
     }
 
     @Test
     public void testSearchNotFound() {
-        System.out.println("---- testSearchNotFound ----");
         MainPage mainPage = new MainPage(this.driver);
-
         SearchResultPage searchResultPage = mainPage.search("virag");
         String bodyText = searchResultPage.getBodyText();
-        //System.out.println(bodyText);
         Assert.assertTrue(bodyText.contains("Not match for this search criteria"));
     }
     
     @Test
     public void testSearchQueriesFound() {
-        System.out.println("---- testSearchQueries ----");
         String[] searchQueries={"sapka","bogre"};  
         for(String searchQuery : searchQueries) {
             //System.out.println("searchQuery: " + searchQuery);  
@@ -73,10 +62,8 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testSearchQueriesNotFound() {
-        System.out.println("---- testSearchQueries ----");
         String[] searchQueries={"auto","motor"};  
         for(String searchQuery : searchQueries) {
-            //System.out.println("searchQuery: " + searchQuery);  
             MainPage mainPage = new MainPage(this.driver);
             SearchResultPage searchResultPage = mainPage.search(searchQuery);
             String bodyText = searchResultPage.getBodyText();
@@ -86,54 +73,40 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testSearchFoundAndClickOne() {
-        System.out.println("---- testSearchFoundAndClickOne ----");
-
         MainPage mainPage = new MainPage(this.driver);
-        //Assert.assertTrue(mainPage.getFooterText().contains("SHOP"));
-
         SearchResultPage searchResultPage = mainPage.search("jegyzetcsipesz");
         String bodyText = searchResultPage.getBodyText();
-        //System.out.println(bodyText);
         Assert.assertTrue(bodyText.contains("ADD TO CART"));
         Assert.assertTrue(bodyText.contains("fa"));
-
         SpecificProductPage specificProductPage = searchResultPage.clickHyperLinkByText("fa jegyzetcsipesz");
         bodyText = specificProductPage.getBodyText();
-        //System.out.println(bodyText);
         Assert.assertTrue(bodyText.contains("Manufacturer"));
     }
 
     @Test
     public void testSearchPageTitle() {
-        System.out.println("---- testSearchFoundAndClickOne ----");
         MainPage mainPage = new MainPage(this.driver);
         SearchResultPage searchResultPage = mainPage.search("jegyzetcsipesz");
-        //System.out.println(specificProductPage.driver.getTitle());
         Assert.assertTrue(searchResultPage.driver.getTitle().contains("Search"));
     }
 
     @Test
     public void testSpecificProductPageTitle() {
-        System.out.println("---- testSpecificProductPageTitle ----");
         MainPage mainPage = new MainPage(this.driver);
         SearchResultPage searchResultPage = mainPage.search("jegyzetcsipesz");
         SpecificProductPage specificProductPage = searchResultPage.clickHyperLinkByText("fa jegyzetcsipesz");
-        //System.out.println(specificProductPage.driver.getTitle());
         Assert.assertTrue(specificProductPage.driver.getTitle().contains("fa jegyzetcsipesz"));
     }
 
     @Test
     public void testLoginPageHeadTitle() {
-        System.out.println("---- testLoginPageHeadTitle ----");
         MainPage mainPage = new MainPage(this.driver);
         LoginPage loginPage = mainPage.clickLoginPageBtn();
-        //System.out.println(loginPage.getHeadTitleText());
         Assert.assertTrue(loginPage.getHeadTitleText().contains("LOGIN"));
     }
 
     @Test
     public void testLoginPageFillAndLoggedAccountPageHeadTitle() {
-        System.out.println("---- testLoginPageFill ----");
         MainPage mainPage = new MainPage(this.driver);
         LoginPage loginPage = mainPage.clickLoginPageBtn();
         LoggedInAccountPage loggedInAccountPage = loginPage.login();
@@ -143,18 +116,15 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testNewsLetterPageTitle() {
-        System.out.println("---- testNewsLetterPageTitle ----");
         MainPage mainPage = new MainPage(this.driver);
         LoginPage loginPage = mainPage.clickLoginPageBtn();
         LoggedInAccountPage loggedInAccountPage = loginPage.login();
         NewsLetterPage newsLetterPage = loggedInAccountPage.getNewsLetterPageLinkClick();
-        //System.out.println(newsLetterPage.getHeadTitleText());
         Assert.assertTrue(newsLetterPage.getHeadTitleText().contains("NEWSLETTER SUBSCRIPTION"));
     }
 
     @Test
     public void testNewsLetterPageSubscribeRadioBtn() {
-        System.out.println("---- testNewsLetterPageSubscribeRadioBtn ----");
         MainPage mainPage = new MainPage(this.driver);
         LoginPage loginPage = mainPage.clickLoginPageBtn();
         LoggedInAccountPage loggedInAccountPage = loginPage.login();
@@ -165,7 +135,6 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testNewsLetterPageUnSubscribeRadioBtn() {
-        System.out.println("---- testNewsLetterPageUnSubscribeRadioBtn ----");
         MainPage mainPage = new MainPage(this.driver);
         LoginPage loginPage = mainPage.clickLoginPageBtn();
         LoggedInAccountPage loggedInAccountPage = loginPage.login();
@@ -176,7 +145,6 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testEditAccountPageFormSend() {
-        System.out.println("---- testEditAccountFormSend ----");
         MainPage mainPage = new MainPage(this.driver);
         LoginPage loginPage = mainPage.clickLoginPageBtn();
         LoggedInAccountPage loggedInAccountPage = loginPage.login();
@@ -187,7 +155,6 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testHoverAccountMenuAndLogOff() {
-        System.out.println("---- testHoverAccountMenuAndLogOff ----");
         MainPage mainPage = new MainPage(this.driver);
         LoginPage loginPage = mainPage.clickLoginPageBtn();
         LoggedInAccountPage loggedInAccountPage = loginPage.login();
@@ -197,7 +164,6 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testHistoryPreviousPage() {
-        System.out.println("---- testHistoryPreviousPage ----");
         MainPage mainPage = new MainPage(this.driver);
         LoginPage loginPage = mainPage.clickLoginPageBtn();
         Assert.assertTrue(loginPage.getHeadTitleText().contains("LOGIN"));
@@ -207,7 +173,6 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testAvoidCookiePopup() {
-        System.out.println("---- testAvoidCookiePopup ----");
         MainPage mainPage = new MainPage(this.driver);
         Assert.assertTrue(mainPage.getCookieDivText().contains("Cookie"));
         driver.manage().addCookie(new Cookie("auroraNanobarAccepted","1"));
@@ -218,7 +183,6 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testBeacPageTitle() {
-        System.out.println("---- testBeacPageTitle ----");
         MainPage mainPage = new MainPage(this.driver);
         BeacPage beacPage = mainPage.clickBeacLink();
         Assert.assertTrue(beacPage.getHeadTitleText().contains("ELTE BEAC"));
@@ -226,7 +190,6 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testBeacPageSortDropDownTitle() {
-        System.out.println("---- testBeacPageSortDropDownTitle ----");
         MainPage mainPage = new MainPage(this.driver);
         BeacPage beacPage = mainPage.clickBeacLink();
         Assert.assertTrue(beacPage.getSortDropDownValue().contains("Low < High"));
@@ -236,7 +199,6 @@ public class SeleniumElteShopTest {
 
     @Test
     public void testBeacPageDragAndDropPize() {
-        System.out.println("---- testBeacPageDragAndDropPize ----");
         MainPage mainPage = new MainPage(this.driver);
         BeacPage beacPage = mainPage.clickBeacLink();
         beacPage = beacPage.dragAndDropPrize();
@@ -250,7 +212,6 @@ public class SeleniumElteShopTest {
         try {
             driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
         } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
