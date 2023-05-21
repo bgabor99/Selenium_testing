cf862efc0a40d00212cdde4341efb1c570a7092c
Gabor Berta <Gabor.Berta@quest.com>
2023-05-21 23:51:43 +0200

    Deleted all unused imports

--- tests/src/test/java/LoggedInAccountPage.java
+++ tests/src/test/java/LoggedInAccountPage.java
@@ -1,20 +1,9 @@
 import java.util.concurrent.TimeUnit;
 
-import javax.swing.Action;
-
-import org.apache.bcel.generic.ACONST_NULL;
-import org.junit.*;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 
 
 class LoggedInAccountPage extends PageBase {
