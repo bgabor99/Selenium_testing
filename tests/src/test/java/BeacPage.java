40717fe7fcb320b11c567a540a84fad949bf997f
Gabor Berta <Gabor.Berta@quest.com>
2023-05-22 11:46:19 +0200

    BeacPage.java: Deleted comment

--- tests/src/test/java/BeacPage.java
+++ tests/src/test/java/BeacPage.java
@@ -46,7 +46,6 @@ class BeacPage extends PageBase {
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return new BeacPage(this.driver);
